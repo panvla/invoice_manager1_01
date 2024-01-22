b8fbd7ecf7419ee6782d094396c67c7751e92768
panvla <pandvlado@gmail.com>
2024-01-22 10:25:58 +0100

    added initial spring boot project.

---
+++ invoice_manager01/src/main/java/com/vladimirpandurov/invoice_manager01/InvoiceManager01Application.java
@@ -0,0 +1,13 @@
+package com.vladimirpandurov.invoice_manager01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceManager01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceManager01Application.class, args);
+	}
+
+}
---
+++ invoice_manager01/src/test/java/com/vladimirpandurov/invoice_manager01/InvoiceManager01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladimirpandurov.invoice_manager01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceManager01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
